f35db8d34c444811351e68b0ce6a5ad75ea19c8d
showkace <ben.401@gmail.com>
2022-02-14 23:48:37 -0800

    initialize

---
+++ script.js
@@ -0,0 +1,9 @@
+let newImg;
+newImg = document.getElementById("gifImg").innerHTML('');
+
+setTimeout (() => {
+      
+    newImg.appendChild( "<a " + "src = ./imgs/hey.png" + "/>" )
+}, 10000);
+
+
